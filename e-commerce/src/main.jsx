c107925b7be40d3ebfb79872b9a5ead79498fa91
unknown <elimila.rodriguez@gmail.com>
2025-07-17 09:35:01 +0200

    Configuración inicial de React Router en main.jsx

--- e-commerce/src/main.jsx
+++ e-commerce/src/main.jsx
@@ -1,11 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './styles/App.scss'; 
-import AppRouter from './router/AppRouter'
-
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App.jsx';
+import './styles/App.scss';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <AppRouter />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>
+);

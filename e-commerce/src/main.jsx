7e4d87d07f81e9aabd13a622525c91863836aa8f
unknown <elimila.rodriguez@gmail.com>
2025-07-17 20:24:56 +0200

    cambios locales en main.jsx antes de mergear develop

--- e-commerce/src/main.jsx
+++ e-commerce/src/main.jsx
@@ -1,17 +1,20 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-import App from './App.jsx';
-import './styles/App.scss';
-import { ProductsProvider } from './context/ProductsContext/ProductsState'; 
+import { UserProvider } from './context/UserContext';
+import { ProductsProvider } from './context/ProductsContext/ProductsState';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <BrowserRouter>
-      <ProductsProvider> {/* ENVUELVE LA APP */}
-        <App />
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <UserProvider>
+      <ProductsProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
       </ProductsProvider>
-    </BrowserRouter>
-  </StrictMode>
+    </UserProvider>
+  </React.StrictMode>
 );
+
 

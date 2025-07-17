91a0ec0e7a990b0126394cd9f0d406222e23306a
unknown <elimila.rodriguez@gmail.com>
2025-07-17 09:43:58 +0200

    Activada navegación con React Router en Header

--- e-commerce/src/components/Header.jsx
+++ e-commerce/src/components/Header.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import '../styles/Header.scss';
-// import { Link } from 'react-router-dom';
-
 import { FiSearch, FiUser, FiShoppingCart } from 'react-icons/fi';
 
 const Header = () => {
@@ -20,25 +19,21 @@ const Header = () => {
         </div>
 
         <div className="header__right">
-          {/* <Link to="/login"> */}
+          <Link to="/login">
             <FiUser className="header__icon" />
-          {/* </Link> */}
-          {/* <Link to="/carrito"> */}
+          </Link>
+          <Link to="/carrito">
             <FiShoppingCart className="header__icon" />
-          {/* </Link> */}
+          </Link>
         </div>
       </div>
 
       <nav className="header__nav">
         <ul className="nav__list">
-          {/* <li><Link to="/">Home</Link></li> */}
-          <li><span>Home</span></li>
-          {/* <li><Link to="/productos">Productos</Link></li> */}
-          <li><span>Productos</span></li>
-          {/* <li><Link to="/nosotros">Nosotros</Link></li> */}
-          <li><span>Nosotros</span></li>
-          {/* <li><Link to="/contacto">Contacto</Link></li> */}
-          <li><span>Contacto</span></li>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/productos">Productos</Link></li>
+          <li><Link to="/nosotros">Nosotros</Link></li>
+          <li><Link to="/contacto">Contacto</Link></li>
         </ul>
       </nav>
     </header>
